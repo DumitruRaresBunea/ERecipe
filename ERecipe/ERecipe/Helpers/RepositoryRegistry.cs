3de2b7c1d458c8ed81c030543e5105ae4ead1dd6
drb <266983@viauc.dk>
2020-09-26 12:35:36 +0200

    Moved all repositories transient registration to a differenct class

---
+++ ERecipe/ERecipe/Helpers/RepositoryRegistry.cs
@@ -0,0 +1,22 @@
+﻿using ERecipe.Services.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ERecipe.Helpers
+{
+    public static class RepositoryRegistry
+    {
+        public static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
+            return
+                services
+                    .AddTransient<ICountryRepository, CountriesRepository>()
+                    .AddTransient<ICategoryRepository, CategoryRepository>()
+                    .AddTransient<IAuthorRepository, AuthorRepository>()
+                    .AddTransient<IIngredientRepository, IngredientRepository>()
+                    .AddTransient<IReviewerRepository, ReviewerRepository>()
+                    .AddTransient<IReviewRepository, ReviewRepository>()
+                    .AddTransient<IStepRepository, StepRepository>()
+                    .AddTransient<IRecipeRepository, RecipeRepository>();
+        }
+    }
+}
